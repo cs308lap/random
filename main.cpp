c3082a8b9e670282724bb55db9a02a25cdba5551
harsh <katakwarharsh@gmail.com>
2021-08-23 14:30:08 +0530

    main updated - step4

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main(){
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    int x=98, y=56;
+    cout<<"The gcd of x and y is "<< nr_gcd(x,y)<< endl;
     return 0;
 }
